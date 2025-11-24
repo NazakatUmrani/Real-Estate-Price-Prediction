2623ddc48975f8c11df20deeae150d070d32b0e1
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2025-11-24 17:01:19 +0500

    Shadcn Themes added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
-    <div className="h-dvh w-dvw overflow-hidden grid place-items-center">
+    <div className="h-dvh w-dvw overflow-hidden grid place-items-center text-primary">
       App
+      <Button>Click Me</Button>
     </div>
   )
 }
