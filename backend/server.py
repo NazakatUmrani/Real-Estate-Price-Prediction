81b571ba84bdf9c7af74a8e9f56f2ad6a3aa9558
Nazakat Umrani <111883142+NazakatUmrani@users.noreply.github.com>
2025-12-02 16:24:37 +0500

    Set Flask app to run on specified port

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import util
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -44,4 +45,5 @@ def predict_price():
 if __name__ == '__main__':
     print("Starting Python Flask Server for Real Estate Price Prediction...")
     util.load_saved_artifacts()
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port)
